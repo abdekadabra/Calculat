41757bb6d544377024f183ce5f267540de3adc31
PC2 <abduda41@gmail.com>
2024-11-18 01:12:11 +0300

    initialize

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Calculat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            double result = number1 - number2;
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
