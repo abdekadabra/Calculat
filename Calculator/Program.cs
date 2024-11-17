5080218567901d57d7b433a0368a02286feb16b3
abdekadabra <abdekadabra@gmail.com>
2024-11-18 00:54:51 +0300

    Initialization

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result = num1 + num2;
+            Console.WriteLine($"Результат сложения: {result}");
+        }
+    }
+}
